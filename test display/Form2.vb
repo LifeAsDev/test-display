5deb8b21652beaacf26f6e1fbc15a354c761c88e
LifeAsDev <angelocraft551@gmail.com>
2025-09-14 16:42:26 -0400

    background color test

--- test display/Form2.vb
+++ test display/Form2.vb
@@ -101,44 +101,140 @@
     End Sub
 
     Private Sub BtnAgregarTexto_Click(sender As Object, e As EventArgs)
-        ' Obtener valores de los NUD del Designer
-        Dim posX = CInt(NUDPosX.Value)
-        Dim posY = CInt(NUDPosY.Value)
-        Dim ancho = CInt(NUDAncho.Value)
-        Dim alto = CInt(NUDAlto.Value)
-        Dim opacidad = CInt(NUDOpacidad.Value)
-        Dim textoContenido = TxtContenido.Text
 
-        If String.IsNullOrWhiteSpace(textoContenido) Then
-            Return
-        End If
-        Dim efectoSeleccionado As Integer = ComboBox1.SelectedIndex
-        Dim objectFitSeleccionado As String = ComboBox2.SelectedItem.ToString()
 
-        ' Enviar al WebView como objeto de texto
-        webForm.AgregarObjetoDisplay(
-            IdGrupo:="grupo1",
-            Id:=Guid.NewGuid().ToString(),
-            Texto:=New TextoConfig With {
-                .Contenido = textoContenido,
-                .Color = "yellow",
-                .FontSize = 48,
-                .FontWeight = "bold",
-                .FontFamily = "Dreams Adventure Co",
-                .Align = "left",
-                .Efecto = efectoSeleccionado
-            },
-            Ancho:=ancho,
-            Alto:=alto,
-            PosX:=posX,
-            PosY:=posY,
-            NivelCapa:=2,
-            Opacidad:=opacidad,
-            Retraso:=0,
-            FadeIn:=400,
-            FadeOut:=0,
-            ObjectFit:=objectFitSeleccionado
-        )
+
+        'fondo
+        webForm.AgregarObjetoDisplay(IdGrupo:="grupo0", Id:=Guid.NewGuid().ToString(), Url:=CopyToTempAndGetUrl("C:\Users\Angelo\Downloads\archivos test v7\fondo.jpeg"),
+Ancho:=1248, Alto:=624,
+PosX:=0, PosY:=0,
+NivelCapa:=1, Opacidad:=100, Retraso:=0, FadeIn:=0, FadeOut:=0, ObjectFit:="fill")
+
+        'mask
+        webForm.AgregarObjetoDisplay(IdGrupo:="grupo1", Id:=Guid.NewGuid().ToString(), Url:=CopyToTempAndGetUrl("C:\Users\Angelo\Downloads\archivos test v7\bossDesign1.webp"),
+Ancho:=1248, Alto:=624,
+PosX:=0, PosY:=0,
+NivelCapa:=2, Opacidad:=100, Retraso:=0, FadeIn:=0, FadeOut:=0)
+
+        'prc
+        webForm.AgregarObjetoDisplay(IdGrupo:="grupo1", Id:=Guid.NewGuid().ToString(), Url:=CopyToTempAndGetUrl("C:\Users\Angelo\Downloads\archivos test v7\v1.webm"),
+Ancho:=450, Alto:=450,
+PosX:=-90, PosY:=100,
+NivelCapa:=5, Opacidad:=100, Retraso:=0, FadeIn:=1000, FadeOut:=0)
+
+        'uda
+        webForm.AgregarObjetoDisplay(IdGrupo:="grupo1", Id:=Guid.NewGuid().ToString(), Url:=CopyToTempAndGetUrl("C:\Users\Angelo\Downloads\archivos test v7\uda.webm"),
+Ancho:=450, Alto:=450,
+PosX:=870, PosY:=100,
+NivelCapa:=5, Opacidad:=100, Retraso:=0, FadeIn:=1000, FadeOut:=0)
+
+
+        'pepsi1
+        webForm.AgregarObjetoDisplay(IdGrupo:="grupo1", Id:=Guid.NewGuid().ToString(), Url:=CopyToTempAndGetUrl("C:\Users\Angelo\Downloads\archivos test v7\pepsi.webm"),
+Ancho:=80, Alto:=80,
+PosX:=124, PosY:=10,
+NivelCapa:=10, Opacidad:=100, Retraso:=0, FadeIn:=1000, FadeOut:=0)
+
+        'pepsi2
+        webForm.AgregarObjetoDisplay(IdGrupo:="grupo1", Id:=Guid.NewGuid().ToString(), Url:=CopyToTempAndGetUrl("C:\Users\Angelo\Downloads\archivos test v7\pepsi.webm"),
+Ancho:=80, Alto:=80,
+PosX:=1044, PosY:=10,
+NivelCapa:=10, Opacidad:=100, Retraso:=0, FadeIn:=1000, FadeOut:=0)
+
+        'animar
+        webForm.AgregarObjetoDisplay(IdGrupo:="grupo1", Id:=Guid.NewGuid().ToString(), Url:=CopyToTempAndGetUrl("C:\Users\Angelo\Downloads\archivos test v7\gato.webm"),
+Ancho:=1248, Alto:=300,
+PosX:=0, PosY:=330,
+NivelCapa:=10, Opacidad:=100, Retraso:=0, FadeIn:=1000, FadeOut:=0)
+
+
+        '0
+        webForm.AgregarObjetoDisplay(IdGrupo:="grupo1", Id:=Guid.NewGuid().ToString(), Texto:=New TextoConfig With {
+        .Contenido = "0",
+        .Color = "white",
+        .FontSize = 330,
+        .FontWeight = "bold",
+        .FontFamily = "Montserrat",
+        .Align = "center",
+        .Efecto = 1},
+    Ancho:=0, Alto:=0,
+    PosX:=480,
+    PosY:=480,
+    NivelCapa:=12,
+    Opacidad:=100,
+    Retraso:=0, FadeIn:=400, FadeOut:=0)
+
+        '0
+        webForm.AgregarObjetoDisplay(IdGrupo:="grupo1", Id:=Guid.NewGuid().ToString(), Texto:=New TextoConfig With {
+        .Contenido = "0",
+        .Color = "white",
+        .FontSize = 330,
+        .FontWeight = "bold",
+        .FontFamily = "Montserrat",
+        .Align = "center",
+        .Efecto = 1},
+    Ancho:=0, Alto:=0,
+    PosX:=760,
+    PosY:=480,
+    NivelCapa:=12,
+    Opacidad:=100,
+    Retraso:=0, FadeIn:=400, FadeOut:=0)
+
+        '12:43
+        webForm.AgregarObjetoDisplay(IdGrupo:="grupo1", Id:=Guid.NewGuid().ToString(), Texto:=New TextoConfig With {
+        .Contenido = "12:43",
+        .Color = "white",
+        .FontSize = 100,
+        .FontWeight = "bold",
+        .FontFamily = "Montserrat",
+        .Align = "center",
+        .Efecto = 1},
+    Ancho:=0, Alto:=0,
+    PosX:=624,
+    PosY:=100,
+    NivelCapa:=12,
+    Opacidad:=100,
+    Retraso:=0, FadeIn:=400, FadeOut:=0)
+
+
+        '' Obtener valores de los NUD del Designer
+        'Dim posX = CInt(NUDPosX.Value)
+        'Dim posY = CInt(NUDPosY.Value)
+        'Dim ancho = CInt(NUDAncho.Value)
+        'Dim alto = CInt(NUDAlto.Value)
+        'Dim opacidad = CInt(NUDOpacidad.Value)
+        'Dim textoContenido = TxtContenido.Text
+
+        'If String.IsNullOrWhiteSpace(textoContenido) Then
+        '    Return
+        'End If
+        'Dim efectoSeleccionado As Integer = ComboBox1.SelectedIndex
+        'Dim objectFitSeleccionado As String = ComboBox2.SelectedItem.ToString()
+
+        '' Enviar al WebView como objeto de texto
+        'webForm.AgregarObjetoDisplay(
+        '    IdGrupo:="grupo1",
+        '    Id:=Guid.NewGuid().ToString(),
+        '    Texto:=New TextoConfig With {
+        '        .Contenido = textoContenido,
+        '        .Color = "yellow",
+        '        .FontSize = 48,
+        '        .FontWeight = "bold",
+        '        .FontFamily = "Dreams Adventure Co",
+        '        .Align = "left",
+        '        .Efecto = efectoSeleccionado
+        '    },
+        '    Ancho:=ancho,
+        '    Alto:=alto,
+        '    PosX:=posX,
+        '    PosY:=posY,
+        '    NivelCapa:=2,
+        '    Opacidad:=opacidad,
+        '    Retraso:=0,
+        '    FadeIn:=400,
+        '    FadeOut:=0,
+        '    ObjectFit:=objectFitSeleccionado
+        ')
     End Sub
 
     Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
